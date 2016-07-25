e1598c7179971f504a4162da8d6eea2b8847c8f2
d-zer0 <daniel.varcas@gmail.com>
2016-07-25 20:35:15 +0100

    second commit

--- browser.rb
+++ browser.rb
@@ -1,13 +1,36 @@
 require 'socket'
+require 'json'
 
 host = 'localhost'
 port = 2000
-path = "/index.html"
 
-request = "GET #{path} HTTP/1.0\r\n\r\n"
+puts "[1]GET or [2]POST?"
+input = gets.chomp
 
-socket = TCPSocket.open(host,port)
-socket.print(request)
-response = socket.read
-headers,body = response.split("\r\n\r\n", 2)
-print body
+case input
+when "1" # GET
+	path = "index.html"
+	# HTTP request
+	request = "GET #{path} HTTP/1.0\r\n\r\n"
+	socket = TCPSocket.open(host, port)
+	socket.print(request)
+	response = socket.read
+	# print response
+	headers, body = response.split("\r\n\r\n", 2)
+	puts headers
+	puts body
+when "2" # POST
+	print "Name: "
+	name = gets.chomp
+	print "Email: "
+	email = gets.chomp
+	hash = { viking: {name: name, email: email} }
+
+	path = "index.html"
+	# HTTP request
+	request = "POST #{path} HTTP/1.0\r\nContent-Length: #{hash.to_json.size}\r\n #{hash.to_json}\r\n\r\n"
+	socket = TCPSocket.open(host, port)
+	socket.print(request)
+	response = socket.read
+	puts response
+end
--- server.rb
+++ server.rb
@@ -1,72 +1,65 @@
 require 'socket'
-
-server = TCPServer.open(2000)
-loop {
-	client = server.accept
-	client.puts(Time.now.ctime)
-
-	input = client.gets
-	request = Request.new(input) # "unitialized constant Request (NameError)"
-
-	verb = request.get_verb
-	path = request.get_path
-
-	if (verb == "GET")
-		path.exist? ? status = "200" : status = "404"
-		client.puts "Status: #{status}"
-		if status == "200"
-			path.open
-			content = path.each {|line| client.puts line}
-			path.close
-			client.puts "Length: #{content.length}"
-		elsif status == "404"
-			client.puts "File not found."
-		else
-			client.puts "Unknown error."
-		end
-	end
-
-	client.puts "Closing the connection. Bye!"
-	client.close
-}
+require 'json'
 
 class Request
 	def initialize(input)
 		@request = input
 	end
 
-	def get_verb
-		# URL that generated this code:
-		# http://txt2re.com/index-ruby.php3?s=GET%20/path/to/file/index.html%20HTTP/1.0&15
-
-		#!/usr/bin/ruby
-		txt=@request
-
-		re1='((?:[a-z][a-z]+))'	# Word 1
-		re=(re1)
-		m=Regexp.new(re,Regexp::IGNORECASE);
-		if m.match(txt)
-			word1=m.match(txt)[1];
-			#puts "("<<word1<<")"<< "\n"
-			word1
-		end
+	def verb
+		@request.split(" ")[0]
 	end
 
-	def get_path
-		# URL that generated this code:
-		# http://txt2re.com/index-ruby.php3?s=GET%20/path/to/file/index.html%20HTTP/1.0&1
-
-		#!/usr/bin/ruby
-		txt=@request
-		re1='.*?'	# Non-greedy match on filler
-		re2='((?:\\/[\\w\\.\\-]+)+)'	# Unix Path 1
-
-		re=(re1+re2)
-		m=Regexp.new(re,Regexp::IGNORECASE);
-		if m.match(txt)
-		    unixpath1=m.match(txt)[1];
-		    #puts "("<<unixpath1<<")"<< "\n"
-		    unixpath1
-		end
+	def path
+		@request.split(" ")[1]
 	end
 end
+
+# SERVER
+
+server = TCPServer.open(2000)
+loop {
+	client = server.accept
+	request = client.read_nonblock(256)
+	client.puts(Time.now.ctime)
+	client.puts "Request: #{request}"
+	request = Request.new(request)
+
+	verb = request.verb
+	path = request.path
+
+	client.puts "Path: #{path}"
+
+	case verb
+		
+# GET REQUEST
+	when "GET"
+		begin
+			file = File.open(path)
+			status = 200
+		rescue
+			status = 404
+		end
+		client.puts "Status: #{status}"
+		case status
+		when 200
+			client.puts "Contents:"
+			client.puts
+			content = file.each {|line| client.puts line}
+			client.puts content
+			client.puts
+			client.puts "File length: #{content.to_s.length}"
+			file.close
+		when 404
+			client.puts "File not found."
+		end
+
+# POST REQUEST
+	when "POST"
+		params = JSON.parse(request.to_s)
+		client.puts params
+	end
+
+	client.puts "Closing the connection. Bye!"
+	client.close
+}
